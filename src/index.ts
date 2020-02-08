5037d22b470259b3a0f769d02cb3b4b8aa03c42b
cramt <mads9220@gmail.com>
2020-02-08 01:33:00 +0100

    wa

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as Discord from "discord.js"
 import SECRET from "./SECRET"
-import { openDBconnection } from "./db";
+import { openDBconnection, startupAddAllGuilds, startupAddAllUsers } from "./db";
 
 let client = new Discord.Client();
 
@@ -11,7 +11,13 @@ client.on("message", async message => {
 })
 
 client.on("ready", async () => {
+    await Promise.all(client.guilds.map(x => x.fetchMembers(undefined, Number.MAX_SAFE_INTEGER)))
     await dbConnPromise;
+    await client.user.setActivity("Gender non-conformity", {
+        type: "STREAMING"
+    })
+    await startupAddAllGuilds(client.guilds.map(x => x.id))
+
 })
 
 client.on("guildCreate", async guild => {
@@ -19,7 +25,7 @@ client.on("guildCreate", async guild => {
 })
 
 client.on("guildMembersChunk", async chunk => {
-
+    await startupAddAllUsers(chunk.map(x => x.id))
 })
 
 client.login(SECRET.DISCORD_TOKEN)
